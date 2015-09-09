ec7f027aa7f50b6c99268f418158167bf01cb3d5
Alley Rubadeau <Alley@Alleys-MacBook-Pro.local>
2015-09-09 09:54:31 -0600

    blog posts working

--- routes/post.js
+++ routes/post.js
@@ -1,48 +1,51 @@
 var express = require('express')
 var router = express.Router()
 var db = require('monk')('localhost/post-demo')
-var postCollection = db.get('post')
+var post = db.get('post')
+var mustache = require('mustache')
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  post.find({}, function (err, docs) {
+    res.render('post/home', {post: docs})
+  })
 });
 
-router.get('/post', function(req, res, next) {
-  res.render('post/index')
-});
+router.post('/', function (req, res, next) {
+  post.insert(req.body)
+  post.find({}, function (err, docs) {
+    res.render('post/home', {post: docs})
+  })
+})
 
 router.get('/post/new', function(req, res, next) {
   res.render('post/new')
 })
 
-router.post('/post', function(req, res, next) {
-  postCollection.insert({name: req.body.post_name})
-  res.redirect('/post')
-})
-
-router.get('/post/:id', function (req, res, next) {
-  postCollection.findOne({_id: req.params.id}, function (err, record) {
-    res.render('post/show', {thePost: record, id: req.params.id})
+router.get('/:id', function (req, res, next) {
+  post.find({_id: req.params.id}, function (err, doc) {
+    res.render('post/show', {post: doc})
   })
 })
 
-router.get('/post/:id/update', function (req, res, next) {
-  postCollection.findOne({_id: req.params.id}, function (err, record) {
-    res.render('post/update', {thePost: record, id: req.params.id})
+router.get('/:id/update', function (req, res, next) {
+  post.findOne({_id: req.params.id}, function (err, doc) {
+    res.render('post/update', {post: doc})
   })
 })
 
-router.post('/post/:id/update', function (req, res, next) {
-  postCollection.update({_id: req.params.id})
+router.post('/:id/update', function (req, res, next) {
+  post.update({_id: req.params.id})
   res.redirect('/post')
 })
 
-router.post('/post/:id/delete', function (req, res, next) {
-  postCollection.remove({_id: req.params.id}, function (err, record) {
+router.post('/:id/delete', function (req, res, next) {
+  post.remove({_id: req.params.id}, function (err, record) {
     res.redirect('/post')
   })
 })
 
-
+//if form is submitted and stuff is not entered right throw error. minimal positioning
+//a few colors, associations and nested resources. comments on posts storing to a database. comments will be nested resource.
+//create a todo list with nested resources.  Create lists and add items to lists as nested resources and without. Items belong to list so in mongo they would be nested resources.  
 
 module.exports = router;
